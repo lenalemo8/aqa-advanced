6b302da58c5f2856d114ef0e0b45313eea11c01f
lenalemo8 <yellowscarf8@gmail.com>
2025-04-21 19:42:26 +0300

    edit task3

--- jsBasics_6/task3.js
+++ jsBasics_6/task3.js
@@ -12,21 +12,44 @@
  * Виведіть об'єкт car3 в консоль.
  */
 
-    const car1 = {
-        brand1:'Honda',
-        model1:'Civic',
-        year1: 2022
-    };
+//1
+    // const car1 = {
+    //     brand1:'Honda',
+    //     model1:'Civic',
+    //     year1: 2022
+    // };
 
-    const car2 = {
-        brand2: 'Ford',
-        model2: 'Mustang',
-        year2: 2020
-    };
+    // const car2 = {
+    //     brand2: 'Ford',
+    //     model2: 'Mustang',
+    //     year2: 2020
+    // };
      
-    const car3 = {
-        ...car1,
-        ...car2
-    };
+    // const car3 = {
+    //     ...car1,
+    //     ...car2
+    // };
 
-    console.log(car3);
+    // console.log(car3);
+
+//2
+
+const car1 = {
+    brand:'Honda',
+    model:'Civic',
+    year: 2022
+};
+
+const car2 = {
+    brand: 'Ford',
+    model: 'Mustang',
+    owner: 'Kat Noise'
+};
+ 
+const car3 = {
+    ...car1,
+    ...car2
+};
+
+console.log(car3);
+
