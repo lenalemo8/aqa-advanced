baf79bb71e6f811fd2be740efb397b4cbc1ce0ac
lenalemo8 <yellowscarf8@gmail.com>
2025-04-26 22:25:39 +0300

    OOP in JS

---
+++ jsBasics_7/Ebook.js
@@ -0,0 +1,38 @@
+//class Ebook
+
+import { Book } from './Book.js';
+
+export class Ebook extends Book {
+  constructor(title, author, year, fileFormat) {
+    super(title, author, year);
+    this.fileFormat = fileFormat;
+  }
+
+  set fileFormat(value) {
+    if (!value || typeof value !== 'string') {
+      throw new Error('The file format must be a non-empty string.');
+    }
+    this._fileFormat = value;
+  }
+
+  get fileFormat() {
+    return this._fileFormat;
+  }
+
+  static createEBookFromBook(book, fileFormat) {
+     return new Ebook(book.title, book.author, book.year, fileFormat);
+    }
+    
+
+  printInfo() {
+    super.printInfo()
+    console.log('-------------------------');
+    console.log(`File format: ${this.fileFormat}`)
+    console.log('-------------------------');
+  }
+
+}
+
+
+
+
